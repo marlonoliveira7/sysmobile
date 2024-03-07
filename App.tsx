6650d9a83b9004678504c691210b241178f49ad0
Marlon Oliveira <marlonoliveiratecnologia@gmail.com>
2024-03-06 22:54:37 -0300

    Pequenas Atualizações

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { LoginScreen } from './src/screens/login';
 import { StyleSheet, Text, View } from 'react-native';
 import { WelcomeScreen } from './src/screens/welcome';
 import { CadastroUsuarioScreen } from './src/screens/cadastroUsuario';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return ( <CadastroUsuarioScreen/> ); 
+  return ( <WelcomeScreen/> ); 
 }
